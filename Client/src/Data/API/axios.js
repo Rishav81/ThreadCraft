0a32ba931593ace8707530d6d7a343fed550a37d
Rishav kumar <rishavkumar4383@gmail.com>
2026-08-22 22:41:28 +0530

    adding backend url

--- Client/src/Data/API/axios.js
+++ Client/src/Data/API/axios.js
@@ -1,40 +1,44 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost/api",
+  baseURL: API_URL,
   withCredentials: true,
 });
 
 export const auth = axios.create({
-  baseURL: "http://localhost/api/auth",
+  baseURL: `${API_URL}/auth`,
   withCredentials: true,
 });
 
 export const userAuth = axios.create({
-  baseURL: "http://localhost/api",
+  baseURL: `${API_URL}/profile`,
   withCredentials: true,
 });
 
 export const productApi = axios.create({
-  baseURL: "http://localhost/api/products",
+  baseURL: `${API_URL}/products`,
   withCredentials: true,
 });
 
 export const cartApi = axios.create({
-  baseURL: "http://localhost/api/cart",
+  baseURL: `${API_URL}/cart`,
   withCredentials: true,
 });
+
 export const orderApi = axios.create({
-  baseURL: "http://localhost/api",
+  baseURL: `${API_URL}/orders`,
   withCredentials: true,
 });
 
 export const paymentApi = axios.create({
-  baseURL: "http://localhost/api/payment",
+  baseURL: `${API_URL}/payment`,
   withCredentials: true,
 });
+
 export const wishlistApi = axios.create({
-  baseURL: "http://localhost/api",
+  baseURL: `${API_URL}/wishlist`,
   withCredentials: true,
 });
 
