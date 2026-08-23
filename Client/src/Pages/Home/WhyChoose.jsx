9174032ccdb39f30bb1dc161f8b53fd03986ab31
Rishav kumar <rishavkumar4383@gmail.com>
2026-08-23 12:38:08 +0530

    Update

--- Client/src/Pages/Home/WhyChoose.jsx
+++ Client/src/Pages/Home/WhyChoose.jsx
@@ -51,11 +51,11 @@ const WhyChoose = () => {
         max-w-7xl
         overflow-hidden
         px-5
-        py-16
+        pb-16
         sm:px-6
-        sm:py-20
+     
         lg:px-8
-        lg:py-24
+       
       "
     >
       <div
